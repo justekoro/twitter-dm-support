046820577c1e925deb013f114755b95dd9b986d7
Mister_KoRo <35098503+korobot@users.noreply.github.com>
2020-10-25 11:32:34 +0100

    :locker: Security issue fixed

--- api/routes/session.js
+++ api/routes/session.js
@@ -3,7 +3,9 @@ const
     umodel = require('../../db/models/users');
 
 module.exports = (req, res) => {
-    console.log(req.body);
+    if (!req.body) {
+        res.redirect("/login?message=Bad%20request");
+    }
     const { username, password } = req.body;
     if (!username || !password) {
         let url = "/login?message=Some%20fields%20are%20missing";
@@ -13,10 +15,24 @@ module.exports = (req, res) => {
     }
     umodel.findOne({ username }).then(result => {
         if (!result) {
-            res.redirect("/login?message=The%20user%20"+username+"%20does%20not%20exist")
+            res.redirect("/login?message=The%20user%20"+username+"%20does%20not%20exist");
+            return;
         }
+        bcrypt.compare(password, result.password, (error, rs) => {
+            if (error) {
+                res.redirect("/login?message=An%20internal%20error%20occured,%20please%20retry");
+                return;
+            }
+            if (!rs) {
+                res.redirect(`/login?message=Wrong%20password&prefill=${username}`);
+                return;
+            }
+            req.session.username = username;
+            res.redirect("/");
+        });
     })
     .catch((e) => {
+        console.error(e);
         res.redirect("/login?message=An%20internal%20error%20occured,%20please%20retry");
     });
 }
