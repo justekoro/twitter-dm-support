6887156087ffe6a2886ffea168dc2fc077982127
Mister_KoRo <35098503+korobot@users.noreply.github.com>
2020-10-25 12:05:00 +0100

    :x: Removed a useless console.log

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,6 @@
 const umodel = require('../../db/models/users');
 
 module.exports = (req, res) => {
-    console.log(req.session.username);
     if (!req.session || !req.session.username) {
         res.redirect("/login?message=You%20must%20login%20to%20access%20the%20main%20page.");
         return;
